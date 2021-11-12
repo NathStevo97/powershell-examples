79b88ec334f72920b5946226d99b43ef61beff8e
NathStevo97 <natstephenson15@gmail.com>
2021-11-12 19:18:55 +0000

    First Commit - Directory Syncing: Source -> Target

---
+++ Directory_Syncing/directory_Sync.ps1
@@ -0,0 +1,31 @@
+$source="C:\Users\natst\Documents\Programming\Powershell_Practice\Directory_Syncing\OldDir" 
+# Directory on System
+#$target="C:\Users\natst\Documents\Programming\Powershell_Practice\Directory_Syncing\NewDir" 
+
+# USB Directory
+$targetLetter = volume | ? drivetype -eq removable | % driveletter # Auto-Find USB Drive Letter
+$target="${targetLetter}:\"
+
+# Get list of files in source and target directories
+$sourceFiles=Get-ChildItem -Path $source -Recurse
+$targetFiles=Get-ChildItem -Path $target -Recurse
+
+$syncMode=2
+
+try {
+    # Identify file content differences for target and source directories; then copy any which aren't in the target dir
+    $diff=Compare-Object -ReferenceObject $sourceFiles -DifferenceObject $targetFiles
+
+    foreach($f in $diff) {
+        if($f.SideIndicator -eq "<=") {
+            $fullSourceObject=$f.InputObject.FullName
+            $fullTargetObject=$f.InputObject.FullName.Replace($source, $target)
+    
+            Write-Host "Attemp to copy the following: " $fullSourceObject
+            Copy-Item -Path $fullSourceObject -Destination $fullTargetObject
+        }
+}
+}
+catch {
+    Write-Error -Message "something bad happened!" -ErrorAction Stop
+}
