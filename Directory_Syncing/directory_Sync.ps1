268090f095f76b4ce64a6cc09cd1d6b66a10204e
NathStevo97 <natstephenson15@gmail.com>
2021-11-16 22:26:20 +0000

    Script Updated: Check directory per filename ONLY and copy all files if target directory is empty

--- Directory_Syncing/directory_Sync.ps1
+++ Directory_Syncing/directory_Sync.ps1
@@ -2,29 +2,36 @@ $source="C:\Users\natst\Documents\Programming\Powershell_Practice\Directory_Sync
 # Directory on System
 $target="C:\Users\natst\Documents\Programming\Powershell_Practice\Directory_Syncing\NewDir" 
 
-# USB Directory
+# USB Directory - Uncomment below two lines and line three if transferring to a USB
 #$targetLetter = volume | ? drivetype -eq removable | % driveletter # Auto-Find USB Drive Letter
 #$target="${targetLetter}:\"
 
 # Get list of files in source and target directories
-$sourceFiles=Get-ChildItem -Path $source -Recurse
-$targetFiles=Get-ChildItem -Path $target -Recurse
+$sourceFiles=Get-ChildItem -Path $source 
+$targetFiles=Get-ChildItem -Path $target # returns null if empty target directory
 
 $syncMode=2
 
 try {
-    # Identify file content differences for target and source directories; then copy any which aren't in the target dir
-    $diff=Compare-Object -ReferenceObject $sourceFiles -DifferenceObject $targetFiles
-    Write-Host $diff
-    foreach($f in $diff) {
-        if($f.SideIndicator -eq "<=") {
-            $filename = $f.InputObject.Basename
-            $fullSourceObject=$f.InputObject.FullName
-            $fullTargetObject=$f.InputObject.FullName.Replace($source, $target)
-    
-            Write-Host "Attempt to copy the following: " $filename
-            Copy-Item -Path $fullSourceObject -Destination $fullTargetObject
+    if ($targetFiles -eq $null) {
+        Write-Host "Target Directory Empty - Transferring All FIles"
+        foreach($file in $sourceFiles) {
+            Write-Host "Attempting to Copy " $file.BaseName
+            Copy-Item -Path $file -Destination $target
         }
+    }
+    else {
+        # Identify file content differences for target and source directories; then copy any which aren't in the target dir
+        $diff=Compare-Object -ReferenceObject $sourceFiles.Name -DifferenceObject $targetFiles.Name
+        foreach($f in $diff) {
+            if($f.SideIndicator -eq "<=") {
+                $fileName = $f.InputObject
+                $sourcePath = -join($source, "\", $fileName)
+                $targetPath = -join($target, "\", $fileName)
+                Write-Host "Attempt to copy the following: " $fileName
+                Copy-Item -Path $sourcePath -Destination $targetPath
+        }
+    }
     }
 }
 catch {
